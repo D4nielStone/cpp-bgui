5edcbc61f9eaf303c4f07db655c4518efc9345ad
D4NL <136137847+D4nielStone@users.noreply.github.com>
2026-04-23 19:11:07 -0300

    Apply dark theme in GLFW example

--- exemples/glfw_gl3/main.cpp
+++ exemples/glfw_gl3/main.cpp
@@ -4,6 +4,8 @@
 int main() {
     // Setup
     bgui::set_up();
+    sm.apply_theme(dark_theme());
+    
     GLFWwindow* window = bgui::set_up_glfw(1280, 720, "BGUI GLFW & gl3 Exemple");
     bgui::set_up_gl3();
     bgui::set_up_freetype();
